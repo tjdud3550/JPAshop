d09ff8162023b7bdfa5543b94c8ef54e42898b04
tjdud3550 <tjdud3550@likelion.org>
2022-03-16 14:50:47 +0900

    Add MemberSeriveTestCode

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.repository;
 
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -9,11 +10,11 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
+
 public class MemberRepository {
-    @PersistenceContext
+
     private EntityManager em;
-
-
 
     public void save(Member member){
         em.persist(member);
@@ -23,18 +24,17 @@ public class MemberRepository {
         return em.find(Member.class, id);
     }
 
-    public List<Member>  findAll(){
-       return em.createQuery("select from Member m", Member.class)
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
-
     }
-
-    public List<Member> findByName(String name){
-        return em.createQuery("select m from Member m where m.name=name", Member.class)
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name",
+                Member.class)
                 .setParameter("name", name)
                 .getResultList();
     }
-
 }
 
 
--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,17 +2,25 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional(readOnly = true) //트랜잭션안에서 값이 변경되어서 꼭필요하다.
+@RequiredArgsConstructor
 public class MemberService {
 
-    @Autowired
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
 
-    //회원가입
+
+
+    /**회원가입*/
+    @Transactional(readOnly = false)
     public  Long join(Member member){
 
         validateDuplicateMember(member);//중복회원 검증
@@ -21,9 +29,20 @@ public class MemberService {
     }
 
     private void validateDuplicateMember(Member member) {
-        //Exception
-
+        List<Member> findMembers =
+                memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
     }
 
     //회원 전체 조회
+
+    public  List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public  Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
 }
