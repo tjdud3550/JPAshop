2a6d8818d03d103f384989da033e2996dba6426d
tjdud3550 <tjdud3550@likelion.org>
2022-03-16 15:39:04 +0900

    Add Order and OrderEntity

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -28,4 +28,27 @@ public class OrderItem {
     private int orderPrice; //주문가격
     private int count; // 주문 수량
 
+    //==생성 메서드==//
+    public  static OrderItem createOrderItem(Item item,int orderPrice,int count){
+      OrderItem orderItem = new OrderItem();
+      orderItem.setItem(item);
+      orderItem.setOrderPrice(orderPrice);
+      orderItem.setCount(count);
+
+      item.removeStock(count);
+      return orderItem;
+
+    }
+    //==비즈니스로직 ==//
+
+    public void cancel() {
+        getItem().addStock(count);
+
+
+    }
+    /**주문 상품 전체 가격 조회 */
+    
+    public  int getTotalPrice(){
+        return getOrderPrice() * getCount();
+    }
 }
